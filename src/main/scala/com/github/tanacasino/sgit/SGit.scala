8c3555af8d0db1f8aab303bdeb2221c7914e34c4
Tomofumi Tanaka <tanacasino@gmail.com>
2014-09-05 00:25:45 +0900

    Add fast version! BUT NOT FAST////
    help me!!!

--- src/main/scala/com/github/tanacasino/sgit/SGit.scala
+++ src/main/scala/com/github/tanacasino/sgit/SGit.scala
@@ -6,9 +6,10 @@ import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.lib.ObjectId
 import org.eclipse.jgit.revwalk.{RevCommit, RevWalk}
 import org.eclipse.jgit.treewalk.TreeWalk
-import org.eclipse.jgit.treewalk.filter.PathFilter
+import org.eclipse.jgit.treewalk.filter.{TreeFilter, PathFilter}
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
 
 
 object SGit {
@@ -181,9 +182,112 @@ class SGit(path: String) {
     }
   }
 
+  // コミットグラフの探索を一回で済ます。そしたら早くなるはず
+  // だいぶイケてないコードだけどもろもろ実験して速くできそうだったら綺麗にする
+  // TODO blob/tree の一覧を出す処理をキャッシュする
+  // TODO すでにコミットが確定したファイル・ディレクトリに対する処理をやめる. blob/tree 一覧出すときもいらないのか・・・ß
   def getLastModifiedCommitFromPathsFast(commit: RevCommit, paths: List[String]): Map[String, String] = {
-    // TODO HELP! Write here fast version
-    getLastModifiedCommitFromPathsSlow(commit, paths)
+    open() { implicit git =>
+      val ite = git.log.add(commit).call().iterator
+      var stop = false
+      var result: Map[String, String] = Map()
+      val lookupPaths: ListBuffer[String] = paths.to[ListBuffer]
+
+      while(ite.hasNext && !stop) {
+        val c = ite.next
+        if(c.getParentCount >= 2) {
+          // merge commit. multiple parents
+          val base = getCommitInfoList(c)
+          val parents = c.getParents.map{ parent =>
+            getCommitInfoList(parent)
+          }
+          var foundPaths = List[String]()
+          lookupPaths.foreach { path =>
+            val baseInfo = base.get(path)
+            val changedThisCommit = parents.map { p =>
+              p.get(path)
+            }.forall { p =>
+              p != baseInfo
+            }
+            if(changedThisCommit && !result.contains(path)) {
+              result += (path -> c.getId.getName)
+              foundPaths = path :: foundPaths
+            }
+          }
+          foundPaths.foreach(f => lookupPaths -= f)
+        } else if (c.getParentCount == 1) {
+          // not merge commit. (only one parent)
+          val base = getCommitInfoList(c)
+          val parent = getCommitInfoList(c.getParent(0))
+          val changes = lookupPaths.filter { path =>
+            val baseInfo = base.get(path)
+            val parentInfo = parent.get(path)
+            (baseInfo, parentInfo) match {
+              case (Some(a), Some(b)) => a.objectId != b.objectId
+              case (Some(a), None) => true
+              case (None, Some(b)) => true
+              case (None, None) => false
+            }
+          }
+          changes.foreach { change =>
+            if(!result.contains(change)) result += (change -> c.getId.getName)
+            lookupPaths -= change
+          }
+        }
+        if(!ite.hasNext && result.size != paths.size) {
+          // First commit in history.
+          lookupPaths.foreach { k =>
+            if(!result.contains(k)) {
+              result += (k -> c.getId.getName)
+            }
+          }
+        }
+        if(result.size == paths.size) {
+          // found commit all paths
+          stop = true
+        }
+      }
+      result
+    }
+  }
+
+  def mismatch(a: List[FileInfo], b: List[FileInfo]): Boolean = {
+    a.forall{ fa =>
+      b.exists(fb => fb.path == fa.path && fb.objectId != fa.objectId)
+    }
+    false
+  }
+
+  case class FileInfo(path: String, objectId: String, objectType: String)
+
+  def getCommitInfoList(commit: RevCommit, path: String = "."): Map[String, FileInfo] = {
+    open() { git =>
+      val repo = git.getRepository
+      using(new TreeWalk(repo)) { tw =>
+        tw.addTree(commit.getTree)
+        if (path != ".") {
+          tw.setFilter(PathFilter.create(path))
+        }
+        val cleanPath = path.stripSuffix("/").split("/")
+        val depth = if (path != ".") cleanPath.size else 0
+
+        @scala.annotation.tailrec
+        def walk(walker: TreeWalk, files: Map[String, FileInfo]): Map[String, FileInfo] = {
+          if(!walker.next()) return files
+          if(depth == walker.getDepth) {
+            walk(walker,
+                 files + (walker.getPathString -> FileInfo(walker.getPathString, walker.getObjectId(0).getName, if(walker.isSubtree) "tree" else "blob")))
+          } else if(walker.isSubtree && walker.getNameString == cleanPath(walker.getDepth)) {
+            tw.enterSubtree
+            walk(walker, files)
+          } else {
+            files  // not correct
+          }
+        }
+
+        walk(tw, Map())
+      }
+    }
   }
 
 }
--- src/test/scala/com/github/tanacasino/sgit/SGitSpec.scala
+++ src/test/scala/com/github/tanacasino/sgit/SGitSpec.scala
@@ -19,4 +19,34 @@ class SGitSpec extends FlatSpec with Matchers {
       emptyStack.pop()
     }
   }
+
+  "SGit.getLatestCommits" should "returns latest commit of file" in {
+    val repositoryPath = "/Users/tanacasino/.gitbucket/repositories/root/git.git"
+    val headCommitId = "cd547b4886c5338a70eb8a674bfc40eac5cab3d9"
+    val sgit = SGit(repositoryPath)
+    val commit = sgit.resolveCommit(headCommitId).get
+    val paths = sgit.listFilesInPath(headCommitId, ".")
+
+    /*
+    val expected = sgit.getLastModifiedCommitFromPathsSlow(commit, paths)
+    val actual = sgit.getLastModifiedCommitFromPathsFast(commit, paths)
+    actual.size should be (expected.size)
+    paths.foreach{ path =>
+      (expected.get(path), actual.get(path)) match {
+        case (Some(e), Some(a)) => if(e != a) println(s"not match: $path, $e, $a")
+        case _ => println(s"not match: $path")
+      }
+    }
+    */
+    /*
+    4 files not match...
+    not match: grep.c, 6bfce93e04d13ecb42008a3cf214cc892f480f0c, 634cd48a8afdd920fa26c8ec3ae43e96c82c81f2
+    not match: builtin-ls-files.c, 80bffaf7fbe09ef62ecb9a6ffea70ac0171b456c, 64586e75af3c84844b80652575a8b63a9612b24a
+    not match: RelNotes, 1e61b7640d09015213dbcae3564fa27ac6a8c151, 806ea701ce3624aa6a89648b6ca5d858703398cb
+    not match: GIT-VERSION-GEN, 1e61b7640d09015213dbcae3564fa27ac6a8c151, 806ea701ce3624aa6a89648b6ca5d858703398cb
+     */
+
+    val result = sgit.getLastModifiedCommitFromPathsFast(commit, paths)
+    result.foreach(println)
+  }
 }
